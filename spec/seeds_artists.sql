62689103b0500b7ff7361a5ee94360687431447b
wimdavies <william.johnston.davies@gmail.com>
2023-04-24 15:57:12 +0100

    Configured artists and albums tables with seed data

---
+++ spec/seeds_artists.sql
@@ -0,0 +1,16 @@
+-- (file: spec/seeds_{table_name}.sql)
+
+-- Write your SQL seed here. 
+
+-- First, you'd need to truncate the table - this is so our table is emptied between each test run,
+-- so we can start with a fresh state.
+-- (RESTART IDENTITY resets the primary key)
+
+TRUNCATE TABLE artists RESTART IDENTITY; -- replace with your own table name.
+
+-- Below this line there should only be `INSERT` statements.
+-- Replace these statements with your own seed data.
+
+INSERT INTO artists (name, genre) VALUES ('The Flaming Lips', 'Alternative Rock');
+INSERT INTO artists (name, genre) VALUES ('Massive Attack', 'Alternative');
+
