79c28d937e953cc642ead130e7f1265005f46222
awdem <wdemontmollin@gmail.com>
2023-04-24 15:10:01 +0100

    began exercise

---
+++ spec/seeds_albums.sql
@@ -0,0 +1,15 @@
+-- (file: spec/seeds_{table_name}.sql)
+
+-- Write your SQL seed here. 
+
+-- First, you'd need to truncate the table - this is so our table is emptied between each test run,
+-- so we can start with a fresh state.
+-- (RESTART IDENTITY resets the primary key)
+
+TRUNCATE TABLE albums RESTART IDENTITY; -- replace with your own table name.
+
+-- Below this line there should only be `INSERT` statements.
+-- Replace these statements with your own seed data.
+
+INSERT INTO albums (title, release_year, artist_id) VALUES ('The Soft Bulletin', 1999, 1);
+INSERT INTO albums (title, release_year, artist_id) VALUES ('Mezzanine', 1998, 2);
