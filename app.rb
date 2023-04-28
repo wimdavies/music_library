9b14949e89304200202a4ba24c679f6d42019c8b
wimdavies <william.johnston.davies@gmail.com>
2023-04-28 10:28:48 +0100

    Setting up for Step 8 exercise

--- app.rb
+++ app.rb
@@ -1,27 +1,42 @@
-require_relative 'lib/database_connection'
-require_relative 'lib/artist_repository'
-require_relative 'lib/album_repository'
+require_relative './lib/album_repository'
+require_relative './lib/artist_repository'
 
-# We need to give the database name to the method `connect`.
-DatabaseConnection.connect('music_library')
+class Application
 
-# Creating new instances of the table repositories:
-artist_repository = ArtistRepository.new
-album_repository = AlbumRepository.new
+  # The Application class initializer
+  # takes four arguments:
+  #  * The database name to call `DatabaseConnection.connect`
+  #  * the Kernel object as `io` (so we can mock the IO in our tests)
+  #  * the AlbumRepository object (or a double of it)
+  #  * the ArtistRepository object (or a double of it)
+  def initialize(database_name, io, album_repository, artist_repository)
+    DatabaseConnection.connect(database_name)
+    @io = io
+    @album_repository = album_repository
+    @artist_repository = artist_repository
+  end
 
-# Printing out the artists from the repository:
-# artist_repository.all.each do |artist|
-#  p artist
-# end
+  def run
+    # "Runs" the terminal application
+    # so it can ask the user to enter some input
+    # and then decide to run the appropriate action
+    # or behaviour.
 
-# Printing out the albums from the repository:
-# album_repository.all.each do |album|
-#   p album
-# end
+    # Use `@io.puts` or `@io.gets` to
+    # write output and ask for user input.
+  end
+end
 
-# Printing out one artist from the ArtistRepository:
-# p artist_repository.find(1)
-
-# Printing out the data of the record with id 3 
-# from the Album Repository:
-p artist_repository.find(3)
+# Don't worry too much about this if statement. It is basically saying "only
+# run the following code if this is the main file being run, instead of having
+# been required or loaded by another file.
+# If you want to learn more about __FILE__ and $0, see here: https://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Variables_and_Constants#Pre-defined_Variables
+if __FILE__ == $0
+  app = Application.new(
+    'music_library',
+    Kernel,
+    AlbumRepository.new,
+    ArtistRepository.new
+  )
+  app.run
+end
